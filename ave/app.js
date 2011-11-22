8d8386f314402a27a02a2f46665a316ba0320523
Mateusz Kuzak <M.T.Kuzak@uva.nl>
2011-11-22 13:11:27 +0100

    No verbose socket.io.

--- ave/app.js
+++ ave/app.js
@@ -39,7 +39,8 @@ app.get('/', function(req, res) {
   });
 });
 
-// io
+// socket.io
+io.set('log level', 1);
 io.sockets.on('connection', function(socket) {
 
 	// socket.on('reloadDb', function(){

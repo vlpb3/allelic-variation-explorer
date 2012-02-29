1a736c3a9f519e9ed274ec0de3a7928119fbb95a
Mateusz Kuzak <M.T.Kuzak@uva.nl>
2012-02-29 14:09:24 +0100

    Cleaned up client side code, especially db part.

--- app.js
+++ app.js
@@ -40,98 +40,19 @@ app.get('/', function(req, res) {
 });
 
 // socket.io
-io.set('log level', 3);
+io.set('log level', 1);
 io.sockets.on('connection', function(socket) {
 
-    socket.on('reloadDb', function(){
-        seqdb.reloadDb(function(err, results) {
-            if(err) console.log(err);
-            else console.log(results);
-            seqdb.getFromRegion(seqdb.Feature, 'mRNA',
-                {chrom: 1, start: 3000, end: 1000000},
-                function(err, doc) {
-                    if (err) console.log(err);
-                    console.dir("mrnas: " + doc[0]);
-                } );
-        });
-    });
-
     socket.on('getData', function(region) {
-        console.log(region);
         seqdb.getRegion(region, function(err, data){
             if (err) throw err;
             else {
-                console.log(data);
                 socket.emit('data', data);
             }
         });
     });
 
-    socket.on('getGeneModels', function(region) {
-        seqdb.getGeneModels(region, function(err, data){
-            if (err) throw err;
-            else {
-                socket.emit('geneModels', data);
-            }
-        });
-    });
-
-    socket.on('getFeatureRegion', function(req) {
-        var name = req.name;
-        var flank = req.flank;
-        seqdb.getFeatureRegion(name, flank, function(err, reg) {
-            if (err) console.log(err);
-            if (reg.start === undefined) {
-                socket.emit('featureNotFound',
-                "Feature has not been found");
-            } else socket.emit('featureRegion', reg);
-        });
-    });
-
-    // socket.on('getFasta', function(region) {
-        //    seqdb.getRefRegion(region, function (err, data) {
-            //      if (err) throw err;
-            //      console.log("fasta region: ");
-            //      console.log(data);
-            //    });
-            // });
 });
-
-// seqdb.getRefRegion({chrom: 1, start: 6484, end: 47889},
-//     function(err, data) {
-//       if (err) throw err;
-//       console.log("refregion: !");
-//       console.log(data);
-//       console.log(data.length);
-// });
-
-//seqdb.annotateCodNCodSNPs(function() {
-//    console.log("annotated coding SNPs")
-//    });
-
-// seqdb.getRefRegion({ start: 14584, end: 39789, chrom: 1 },
-//   function(err, data) {
-//     if (err) throw err;
-//     console.log("ref seq: ");
-//     console.log(data);
-//   });
-
-// seqdb.importRefSeq(function(err, res) {
-//   if (err) throw err;
-//   console.log(res);
-// })
-
-// // use stalker to watch the database directory
-// stalker.watch('./data/imports', {buffer: 5000},
-//     function(err, files) {
-//         if (err) throw err;
-//         seqdb.onDbFilesAdded(files);
-//     },
-//     function(err, files) {
-//         if (err) throw err;
-//         seqdb.onDbFilesRemoved(files);
-//     }
-// );
 
 app.listen(3000);
 console.log("Express server listening on port %d in %s mode",

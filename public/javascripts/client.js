1d819140372922e3b781b05c1b71ee07b46e7ea5
mateusz kuzak <mateusz.kuzak@gmail.com>
2014-06-11 10:53:00 +0200

    Turn on regex in search.

--- public/javascripts/client.js
+++ public/javascripts/client.js
@@ -365,8 +365,8 @@
 
       // apply the filter connected with search boxes
       dTable.columns().eq( 0 ).each( function(colIdx) {
-        $('.filterTable tfoot input').eq(colIdx).on('change', function() {
-          dTable.column(colIdx).search(this.value).draw();
+        $('.filterTable tfoot input').eq(colIdx).on('keyup click', function() {
+          dTable.column(colIdx).search(this.value, true).draw();
         });
       });
     }
